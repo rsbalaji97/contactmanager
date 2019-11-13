28c390ef42de1fe4e1093d17d88faa4bd3a1b56a
rsbalaji97 <rsb.balaji97@gmail.com>
2019-11-13 19:17:14 +0530

    Updates

---
+++ precache-manifest.f23808e3c2172d0f576184ff41b6f0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9e090149f5e68e851d325ea0ded9b1",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "74bcc3308c04d4d8bbd4",
+    "url": "/contactmanager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "17e97551017127acc701",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "74bcc3308c04d4d8bbd4",
+    "url": "/contactmanager/static/js/2.0bc17292.chunk.js"
+  },
+  {
+    "revision": "17e97551017127acc701",
+    "url": "/contactmanager/static/js/main.a16570ba.chunk.js"
+  },
+  {
+    "revision": "52a8038624ffe69f6d88",
+    "url": "/contactmanager/static/js/runtime-main.9acb0f81.js"
+  }
+]);
